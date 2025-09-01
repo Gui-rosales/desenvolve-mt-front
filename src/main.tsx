67fca884ccb24fe15845fd0e25f7d4c0be409f04
Gui-rosales <guilhermelexandre456@gmail.com>
2025-09-01 08:46:03 -0400

    feat: implementa inicialização assíncrona da aplicação com suporte a mocks de API para ambiente de teste

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,25 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+async function enableApiMockingOnBrowser() {
+  const { worker } = await import('./app/mocks/browser-interceptor.ts');
+  console.log('Iniciando mock de API');
+  await worker.start();
+}
+
+async function initializeApp() {
+  if (import.meta.env.VITE_NODE_ENV === 'test') {
+    console.log('Iniciando mock de API');
+    await enableApiMockingOnBrowser();
+  }
+}
+
+initializeApp().then(() => {
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+});
