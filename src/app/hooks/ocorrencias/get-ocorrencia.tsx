c0bd5c91381992258ce81a7b1aac623c1ac91354
Gui-rosales <guilhermelexandre456@gmail.com>
2025-09-07 09:41:42 -0400

    feat: atualiza a função getOcorrenciasById para ordenar as ocorrências pela data em ordem decrescente

--- src/app/hooks/ocorrencias/get-ocorrencia.tsx
+++ src/app/hooks/ocorrencias/get-ocorrencia.tsx
@@ -4,7 +4,11 @@ import { useQuery } from '@tanstack/react-query';
 export function getOcorrenciasById(id: number, enabled: boolean = true) {
   return useQuery({
     queryKey: ['get-ocorrencias', id],
-    queryFn: () => ocorrenciaService.getOcorrenciasById(id),
+    queryFn: async () => {
+      const data = await ocorrenciaService.getOcorrenciasById(id);
+      data.sort((a, b) => new Date(b.data).getTime() - new Date(a.data).getTime());
+      return data;
+    },
     enabled,
   });
 }
